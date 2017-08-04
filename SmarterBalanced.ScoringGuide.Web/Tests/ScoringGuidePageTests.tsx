9ba00ae0ec05726d835718bef65d184c6f9295fd
Brooks Mikkelsen <bmikkelsen22@gmail.com>
2017-08-04 12:15:59 -0700

    Added ability to test react components

---
+++ SmarterBalanced.ScoringGuide.Web/Tests/ScoringGuidePageTests.tsx
@@ -0,0 +1,12 @@
+﻿import * as React from 'react';
+import { shallow } from 'enzyme';
+import * as ScoringGuidePage from '../Scripts/ScoringGuidePage';
+import * as ItemViewerFrame from '../Scripts/ItemViewerFrame';
+
+describe("<ScoringGuidePage />", () => {
+    
+    it("Should render an ItemFrame", () => {
+        const wrapper = shallow(<ScoringGuidePage.ScoringGuidePage />);
+        expect(wrapper.find(ItemViewerFrame.ItemFrame)).toHaveLength(1);
+    });
+});
