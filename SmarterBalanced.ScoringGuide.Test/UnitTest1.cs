d64775dff8e64e2341235b1315e2c844e386ae90
Brooks Mikkelsen <bmikkelsen22@gmail.com>
2017-08-04 13:14:30 -0700

    Added new unit test project, example unit test

---
+++ SmarterBalanced.ScoringGuide.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace SmarterBalanced.ScoringGuide.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal("Testing", "Testing");
+        }
+    }
+}
