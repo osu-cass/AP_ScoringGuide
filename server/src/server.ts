ea0499af326378b062b7268ff37de5a81553faab
Brooks Mikkelsen <bmikkelsen22@gmail.com>
2017-08-29 10:23:46 -0700

    map /scoringguide to index.html

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,5 +5,10 @@ import pdfRoute from './routes/pdf';
 const port = process.env.PORT || 3000;
 const app = Express();
 app.use(Express.static(Path.join(__dirname, '../../client/dist')));
+
 app.post('/pdf', pdfRoute);
+app.get('/ScoringGuide', (req, res) => {
+    res.sendFile(Path.join(__dirname, '../../client/dist/index.html'));
+})
+
 app.listen(port, () => console.log('Server started on port ', port));
