ee4b971a9124580309eb2458470042f70024c852
Brooks Mikkelsen <bmikkelsen22@gmail.com>
2017-09-01 09:53:14 -0700

    Changed naming scheme of server side react components

--- server/src/PdfGenerator.tsx
+++ server/src/PdfGenerator.tsx
@@ -1,11 +1,10 @@
 import * as React from 'react';
 import * as ReactDOMServer from 'react-dom/server';
 import { Item } from './models';
-import * as Pdf from './components/Pdf';
-import * as Header from './components/Header';
+import { PdfComponent } from './components/Pdf';
 
 export class HtmlRenderer {
     static renderBody(items: Item[]) {
-        return ReactDOMServer.renderToString(<Pdf.Component />);
+        return ReactDOMServer.renderToString(<PdfComponent />);
     }
 }
