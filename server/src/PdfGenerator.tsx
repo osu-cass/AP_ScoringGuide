db41a0981344d93774a56d14ec3ab442665a6b5f
Brooks Mikkelsen <bmikkelsen22@gmail.com>
2017-09-01 10:08:57 -0700

    Factor pdf generator out of express route function

--- server/src/PdfGenerator.tsx
+++ server/src/PdfGenerator.tsx
@@ -1,10 +1,27 @@
 import * as React from 'react';
 import * as ReactDOMServer from 'react-dom/server';
 import { Item } from './models';
-import { PdfComponent } from './components/Pdf';
+import { PdfComponent } from './components/PdfComponent';
+import { Stream } from "stream";
+const wkhtmltopdf = require('wkhtmltopdf');
 
 export class HtmlRenderer {
     static renderBody(items: Item[]) {
         return ReactDOMServer.renderToString(<PdfComponent />);
+    }
+}
+
+export class PdfGenerator {
+    static generate(html: string, title: string) {
+        const urlTitle = encodeURIComponent(title);
+        const port = process.env.PORT || 3000;
+        const options = {
+            headerHtml: 'http://localhost:' + port + '/pdf-header.html?title=' + urlTitle,
+            headerSpacing: 5,
+            footerSpacing: 5,
+            footerHtml: 'http://localhost:' + port + '/pdf-footer.html?title=' + urlTitle,
+            marginBottom: '13mm'
+        };
+        return wkhtmltopdf(html, options) as Stream;
     }
 }
--- server/src/routes/pdf.ts
+++ server/src/routes/pdf.ts
@@ -1,6 +1,5 @@
-const wkhtmltopdf = require('wkhtmltopdf');
 import { Request, Response } from 'express';
-import { HtmlRenderer } from "../PdfGenerator";
+import { HtmlRenderer, PdfGenerator } from "../PdfGenerator";
 import { ItemCapture } from "../ItemCapture";
 import { ScreenshotManager } from "../ScreenshotManager";
 
@@ -19,15 +18,6 @@ export default async function post(req: Request, res: Response) {
 
     const htmlString = HtmlRenderer.renderBody([]);
     const title = 'Grade 5 Mathematics';
-    const urlTitle = encodeURIComponent(title);
-    const port = process.env.PORT || 3000;
-    const options = {
-        headerHtml: 'http://localhost:' + port + '/pdf-header.html?title=' + urlTitle,
-        headerSpacing: 5,
-        footerSpacing: 5,
-        footerHtml: 'http://localhost:' + port + '/pdf-footer.html?title=' + urlTitle,
-        marginBottom: '13mm'
-    };
     res.type('application/pdf');
-    wkhtmltopdf(htmlString, options).pipe(res);
+    PdfGenerator.generate(htmlString, title).pipe(res);
 }
