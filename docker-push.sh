0421e7a9212bb0c84d217c7d323fdf7e593ddd29
Brooks Mikkelsen <bmikkelsen22@gmail.com>
2017-09-15 15:45:49 -0700

    update docker push for testing

--- docker-push.sh
+++ docker-push.sh
@@ -3,4 +3,4 @@
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 docker build -t scoring-guide .
 docker tag scoring-guide:latest osucass/scoring-guide:$BRANCH
-docker push osucass/scoring-guide:$BRANCH
+docker push osucass/scoring-guide:test
