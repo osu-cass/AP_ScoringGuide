cc88f5f02a7ec6ff9b92027f24adb51277dbe7c3
Brooks Mikkelsen <bmikkelsen22@gmail.com>
2017-09-15 16:58:59 -0700

    switched settings back after testing

--- docker-push.sh
+++ docker-push.sh
@@ -2,5 +2,5 @@
 
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 docker build -t scoring-guide .
-docker tag scoring-guide:latest osucass/scoring-guide:test
-docker push osucass/scoring-guide:test
+docker tag scoring-guide:latest osucass/scoring-guide:$BRANCH
+docker push osucass/scoring-guide:$BRANCH
