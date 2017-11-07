401a9fbb61180c6458c2ea0d055eaf8128509750
Brooks Mikkelsen <bmikkelsen22@gmail.com>
2017-11-07 13:35:58 -0800

    changed config files typescript files

--- config/production.ts
+++ config/production.ts
@@ -1,6 +1,10 @@
-{
+const production = {
     "sampleItemsApi": "http://is-score.cass.oregonstate.edu",
     "itemViewerServiceApi": "http://ivs.smarterbalanced.org",
     "screenshotPageWidth": 700,
-    "port": 3000
+    "port": 3000,
+    "catCode": "CAT",
+    "performanceItemsCode": "PT"
 }
+
+export default production;
