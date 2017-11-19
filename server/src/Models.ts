a7640da1af80521fe341c822c72d1dae3ae7c5fa
Alex Lepinski <alexjlepinski@gmail.com>
2017-11-18 18:04:24 -0800

    removed sb-component models

--- server/src/Models.ts
+++ server/src/Models.ts
@@ -1,4 +1,3 @@
-import * as GradeLevels from './GradeLevels';
 
 export interface ItemGroup {
     passage?: ItemView;
@@ -25,79 +24,5 @@ export enum ViewType {
     html
 }
 
-export interface ItemViewModel {
-    bankKey: number;
-    itemKey: number;
-    title: string;
-    grade: GradeLevels.GradeLevels;
-    gradeLabel: string;
-    subjectCode: string;
-    subjectLabel: string;
-    claimCode: string;
-    claimLabel: string;
-    target: string;
-    targetId: string;
-    interactionTypeCode: string;
-    interactionTypeLabel: string;
-    isPerformanceItem: boolean;
-    domain: string;
-    depthOfKnowledge: string;
-    commonCoreStandardId: string;
-}
 
-export interface AboutItemViewModel {
-    itemCardViewModel: ItemViewModel;
-    sampleItemScoring: SampleItemScoring;
-    targetDescription: string;
-    depthOfKnowledge: string;
-    commonCoreStandardsDescription: string;
-    educationalDifficulty: string;
-    evidenceStatement: string;
-    associatedItems: string;
-}
 
-export interface Rubric {
-    language: string;
-    rubricEntries: RubricEntry[];
-    samples: Sample[];
-}
-
-export interface RubricEntry {
-    scorepoint: string;
-    name: string;
-    value: string;
-}
-
-export interface Sample {
-    maxValue: string;
-    minValue: string;
-    sampleResponses: SampleResponse[];
-}
-
-export interface SampleResponse {
-    purpose: string;
-    scorePoint: string;
-    name: string;
-    sampleContent: string;
-}
-
-export interface Subject {
-    code: string;
-    label: string;
-    shortLabel: string;
-}
-
-export interface SampleItemScoring {
-    answerKey: string;
-    hasMachineRubric: boolean;
-    scoringOptions: SmarterAppOption[];
-    rubrics: Rubric[];
-}
-
-export interface SmarterAppOption {
-    name: string;
-    value: string;
-    feedback: string;
-    language: string;
-    answer: string;
-}
