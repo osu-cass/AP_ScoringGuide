9b29ea97726cb476f652cd53c35c9b744d1358ee
Alex Lepinski <alexjlepinski@gmail.com>
2017-12-14 11:48:02 -0800

    Added tslint, prettier, husky, tslint-config. Updated package.json scripts and license. Added dependent testing files. updated tsconfig to have tslint

--- client/webpack.config.vendor.js
+++ client/webpack.config.vendor.js
@@ -19,10 +19,21 @@ module.exports = (env) => {
             ]
         },
         entry: {
-            vendor: ['bootstrap', 'bootstrap/dist/css/bootstrap.css', "@osu-cass/sb-components/lib/sb-components.css", 'font-awesome/css/font-awesome.css', 'event-source-polyfill', 'react', 'react-dom', 'react-router-dom', 'jquery'],
+            vendor: [
+                "bootstrap",
+                "bootstrap/dist/css/bootstrap.css",
+                "@osu-cass/sb-components,
+                "@osu-cass/sb-components/lib/sb-components.css",
+                "font-awesome/css/font-awesome.css",
+                "event-source-polyfill",
+                "react", 
+                "react-dom", 
+                "react-router-dom", 
+                "jquery"
+            ],
         },
         output: {
-            path: path.join(__dirname,  "../server/public/client/"),
+            path: path.join(__dirname, bundleOutputDir),
             publicPath: '/client/',
             filename: '[name].js',
             library: '[name]_[hash]',
@@ -31,7 +42,7 @@ module.exports = (env) => {
             extractCSS,
             new webpack.ProvidePlugin({ $: 'jquery', jQuery: 'jquery' }), // Maps these identifiers to the jQuery package (because Bootstrap expects it to be a global variable)
             new webpack.DllPlugin({
-                path: path.join(__dirname,  "../server/public/client/", '[name]-manifest.json'),
+                path: path.join(__dirname, bundleOutputDir , '[name]-manifest.json'),
                 name: '[name]_[hash]'
             }),
             new webpack.DefinePlugin({
