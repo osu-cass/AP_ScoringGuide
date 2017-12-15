f552cecfc763cc4009fcb525d9ec8861817f008f
Brooks Mikkelsen <bmikkelsen22@gmail.com>
2017-12-15 13:56:37 -0800

    css for pdf only, compile it with less

--- server/src/PdfGenerator.tsx
+++ server/src/PdfGenerator.tsx
@@ -15,7 +15,7 @@ export class HtmlRenderer {
                 subject={subject}
                 grade={grade}
                 ivsBaseUrl={ITEM_VIEWER_SERVICE_API}
-                cssUrl={path.join(__dirname, '../public/client/vendor.css')}
+                cssUrl={path.join(__dirname, '../public/pdf/pdf.css')}
                 displayTitlePage={titlePage} 
                 displayScoreInfo={scoringInfo} />
         );
