7159fea8202ee49a1baa6c681b93d1e80f7b06ea
Alex Lepinski <alexjlepinski@gmail.com>
2018-02-14 12:23:47 -0800

    added fonts and promise

--- client/src/Index.tsx
+++ client/src/Index.tsx
@@ -1,8 +1,13 @@
+import "./Styles/bundle.less";
+import "typeface-pt-sans-caption/index.css";
+import "typeface-pt-serif/index.css";
+import "typeface-pt-serif-caption/index.css";
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import "./Styles/bundle.less";
 import { routes } from './Routes';
 import { BrowserRouter } from 'react-router-dom';
+// tslint:disable-next-line:no-var-requires no-require-imports
+require("es6-promise/auto");
 
 function renderApp() {
     ReactDOM.render(
