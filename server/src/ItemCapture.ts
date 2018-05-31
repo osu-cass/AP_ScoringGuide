e8d861f74625e1ecf915c7a1d07847d55a157b9c
Brooks Mikkelsen <bmikkelsen22@gmail.com>
2018-05-31 15:54:29 -0700

    working on testing item capture

--- server/src/ItemCapture.ts
+++ server/src/ItemCapture.ts
@@ -5,19 +5,30 @@ import {
     ItemPdfModel,
     PdfViewType
 } from "@osu-cass/sb-components";
-const puppeteer = require("puppeteer");
+import * as Puppeteer from "puppeteer";
 
-const { SCREENSHOT_WIDTH } = process.env;
+const { SCREENSHOT_WIDTH, ITEM_VIEWER_SERVICE_API } = process.env;
+
+export interface ContentDimensions {
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+}
+
+export interface QuestionDimensions extends ContentDimensions {
+    itemId: string;
+}
 
 export class ItemCapture {
-    browser: any;
+    browser: Puppeteer.Browser;
 
     async launchBrowser() {
-        this.browser = await puppeteer.launch({ args: ["--no-sandbox"] });
+        this.browser = await Puppeteer.launch({ args: ["--no-sandbox"] });
         console.log("chrome url: ", this.browser.wsEndpoint());
     }
 
-    getIdString(paths: ItemGroupModel) {
+    getIdString(paths: ItemGroupModel): string {
         const ids: string[] = [];
         paths.questions.forEach(p => {
             if (p.view.type === PdfViewType.picture) {
@@ -32,17 +43,24 @@ export class ItemCapture {
         return ids.join(",");
     }
 
-    async takeScreenshots(itemData: ItemGroupModel) {
-        const page = await this.browser.newPage();
-        const idsString = this.getIdString(itemData);
-        await page.goto(
-            `http://ivs.smarterbalanced.org/items?ids=${idsString}&isaap=TDS_SLM1`
-        );
+    /**
+     * Updates the viewport's size to be the size of the contents
+     * (header height + item view height) when the width is as specified
+     *
+     * @param page Puppeteer page object
+     * @param iframe puppeteer iframe object containing the item view
+     * @param pageWidth desired page width
+     */
+    public static async updateViewportSize(
+        page: Puppeteer.Page,
+        iframe: Puppeteer.Frame,
+        pageWidth: number
+    ) {
         await page.setViewport({
-            width: SCREENSHOT_WIDTH,
+            width: pageWidth,
             height: 1000
         });
-        const iframe = await page.frames()[1];
+
         await iframe.waitForSelector(".grouping", {
             timeout: 5000
         });
@@ -57,41 +75,33 @@ export class ItemCapture {
         });
 
         await page.setViewport({
-            width: SCREENSHOT_WIDTH,
+            width: pageWidth,
             height: groupingHeight + headerHeight
         });
+    }
 
-        // passage
-        if (
-            itemData.passage &&
-            itemData.passage.type === PdfViewType.picture &&
-            !itemData.passage.captured
-        ) {
-            const passageRect = await iframe.evaluate(() => {
-                const passage = document.querySelector(".thePassage");
-                const rect = passage.getBoundingClientRect();
+    public static async getPassageRect(iframe: Puppeteer.Frame): Promise<ContentDimensions> {
+        return iframe.evaluate(() => {
+            const passage = document.querySelector(".thePassage");
+            const rect = passage.getBoundingClientRect();
 
-                return {
-                    x: scrollX + rect.left,
-                    y: scrollY + rect.top,
-                    width: rect.width,
-                    height: rect.height
-                };
-            });
+            return {
+                x: scrollX + rect.left,
+                y: scrollY + rect.top,
+                width: rect.width,
+                height: rect.height
+            };
+        });
+    }
 
-            await page.screenshot({
-                path: itemData.passage.picturePath,
-                clip: passageRect
-            });
-            itemData.passage.captured = true;
-        }
-
-        // questions
-        const itemRects = await iframe.evaluate(() => {
+    public static async getQuestionRects(
+        iframe: Puppeteer.Frame
+    ): Promise<QuestionDimensions[]> {
+        return iframe.evaluate(() => {
             const headerHeight = document.querySelector(".questionNumber")
                 .clientHeight;
             const elements = document.querySelector(".theQuestions").children;
-            const rects = [];
+            const rects: QuestionDimensions[] = [];
             // tslint:disable-next-line:prefer-for-of
             for (let i = 0; i < elements.length; i++) {
                 const boundingClientRect = elements[i].getBoundingClientRect();
@@ -107,6 +117,35 @@ export class ItemCapture {
 
             return rects;
         });
+    }
+
+    async takeScreenshots(itemData: ItemGroupModel): Promise<ItemGroupModel> {
+        const page = await this.browser.newPage();
+        const idsString = this.getIdString(itemData);
+        await page.goto(
+            `${ITEM_VIEWER_SERVICE_API}/items?ids=${idsString}&isaap=TDS_SLM1`
+        );
+        const iframe = await page.frames()[1];
+
+        await ItemCapture.updateViewportSize(page, iframe, Number(SCREENSHOT_WIDTH));
+
+        // passage
+        if (
+            itemData.passage &&
+            itemData.passage.type === PdfViewType.picture &&
+            !itemData.passage.captured
+        ) {
+            const passageRect = await ItemCapture.getPassageRect(iframe);
+
+            await page.screenshot({
+                path: itemData.passage.picturePath,
+                clip: passageRect
+            });
+            itemData.passage.captured = true;
+        }
+
+        // questions
+        const itemRects = await ItemCapture.getQuestionRects(iframe);
 
         // tslint:disable-next-line:prefer-for-of
         for (let i = 0; i < itemRects.length; i++) {
--- server/src/__tests__/ItemCapture.test.ts
+++ server/src/__tests__/ItemCapture.test.ts
@@ -1,6 +1,7 @@
 import * as Mocks from "./Mocks";
 import { ItemCapture } from "../ItemCapture";
 import { PdfViewType } from "@osu-cass/sb-components";
+import * as Puppeteer from "puppeteer";
 
 describe("ItemCapture.getIdString", () => {
     it("gets single id", () => {
@@ -27,17 +28,54 @@ describe("ItemCapture.getIdString", () => {
         });
         expect(result).toEqual("182-1234");
     });
+});
 
-    it("passage only html", () => {
-        const itemCapture = new ItemCapture();
-        const result = itemCapture.getIdString({
-            passage: {
-                id: "182-1234",
-                captured: false,
-                type: PdfViewType.html
-            },
-            questions: []
+describe("ItemCapture.setViewportHeight", async () => {
+    const browser = await Puppeteer.launch({ args: ["--no-sandbox"] });
+    const page = await browser.newPage();
+    await page.goto(
+        "http://ivs.smarterbalanced.org/items?ids=187-2558&isaap=TDS_SLM1"
+    );
+    const iframe = await page.frames()[1];
+
+    it("sets the correct width", async () => {
+        await ItemCapture.updateViewportSize(page, iframe, 800);
+        const width = await page.evaluate(() => window.outerWidth);
+        expect(width).toEqual(800);
+    });
+
+    it("viewport contains grouping", async () => {
+        await ItemCapture.updateViewportSize(page, iframe, 800);
+        const groupingHeight: number = await iframe.evaluate(() => {
+            return document.querySelector(".grouping").clientHeight;
         });
-        expect(result).toEqual("");
+        const windowHeight: number = await page.evaluate(
+            () => window.outerHeight
+        );
+        expect(windowHeight).toBeGreaterThan(groupingHeight);
+    });
+});
+
+describe("ItemCapture.getPassageRect", async () => {
+    const browser = await Puppeteer.launch({ args: ["--no-sandbox"] });
+    const page = await browser.newPage();
+    await page.goto(
+        "http://ivs.smarterbalanced.org/items?ids=187-2558&isaap=TDS_SLM1"
+    );
+    const iframe = await page.frames()[1];
+    await iframe.waitForSelector(".grouping", {
+        timeout: 5000
+    });
+    await new Promise(resolve => setTimeout(resolve, 200));
+
+    it("gets the passage size", async () => {
+        // TODO: why isnt this running
+        const result = await ItemCapture.getPassageRect(iframe);
+        expect(result).toEqual({
+            x: 0,
+            y: 123,
+            height: 0,
+            width: 0
+        });
     });
 });
